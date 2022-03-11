ab94482cf74a023908ade0cfa6fe0127baa54e0d
Jaseela99 <jaseelachittur@gmail.com>
2022-03-11 14:40:12 +0530

    added home

--- imdbclone/src/components/Home.js
+++ imdbclone/src/components/Home.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  return <Container>Home</Container>;
 }
 
-export default Home
+export default Home;
+
+//main content
+const Container = styled.main`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+
+  //adding background image
+  &:before {
+    //background : image position size  repeat attachment
+    background: url("/images/home-background.png") center / cover no-repeat
+      fixed;
+    content: "";
+    position: absolute;
+
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`;
