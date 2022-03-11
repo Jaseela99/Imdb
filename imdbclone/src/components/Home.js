4569ce6f0df270dbf55c93450689b70dc5b5e1e8
Jaseela99 <jaseelachittur@gmail.com>
2022-03-12 00:46:28 +0530

    added veiwers

--- imdbclone/src/components/Home.js
+++ imdbclone/src/components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import ImageSlider from "./ImageSlider";
+import Veiwers from "./Veiwers";
 
 function Home() {
-  return <Container>
-      <ImageSlider/>
-  </Container>;
+  return (
+    <Container>
+      <ImageSlider />
+      <Veiwers />
+    </Container>
+  );
 }
 
 export default Home;
@@ -15,7 +19,7 @@ const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
   position: relative;
-  overflow-x:hidden;
+  overflow-x: hidden;
 
   //adding background image
   &:before {
--- imdbclone/src/components/ImageSlider.js
+++ imdbclone/src/components/ImageSlider.js
@@ -1,75 +1,75 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 //react slick: library for creating carousels
 //slick carousel :A slideshow component for cycling through elements—images or slides of text—like a carousel.
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css"
-import Slider from "react-slick"
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 function ImageSlider() {
   let settings = {
-      //to show dots
-      dots:true,
-      //slide duratiom
-      speed:500,
-      slidesToShow:1,
-      //to show infinitely
-      infinite:true,
-      slidesToScroll:1,
-      autoplay:true,
+    //to show dots
+    dots: true,
+    //slide duratiom
+    speed: 500,
+    slidesToShow: 1,
+    //to show infinitely
+    infinite: true,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
 
-
-  }
-  
-    return (
+  return (
     <Carousel {...settings}>
-        <Wrap>
-            <img src="/images/slider-badag.jpg"alt=""/>
-        </Wrap>
-        <Wrap>
-            <img src="/images/slider-badging.jpg"alt=""/>
-        </Wrap>
-        
-        </Carousel>
-  )
+      <Wrap>
+        <img src="/images/slider-badag.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-badging.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scale.jpg" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-scales.jpg" alt="" />
+      </Wrap>
+    </Carousel>
+  );
 }
 
-export default ImageSlider
+export default ImageSlider;
 
-const Carousel=styled(Slider)`
+const Carousel = styled(Slider)`
+  margin-top: 20px;
 
-margin-top:20px;
-
-ul li button {
-    &:before{
-        font-size:10px;
-        color:rgb(150,158,171);
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
     }
-}
-//button's default color has been changed
-li.slick-active button:before{
-    color:white;
-}
-.slick-list{
-    overflow:visible;
-}
-button{
-    z-index:1;
-}
-
-`
-const Wrap =styled.div`
-img{
-    border:4px solid transparent;
-   width:100%;
-   height:100% ;
-   bordr-radius:6px;
-   box-shadow:rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-   rgb(0 0 0 /73%) 0 16px 10px -10px;
-   transition-duration:300ms;
-   &:hover{
-       border:4px solid rgba(249,249,249,0.8);
-
-   }
-}
-`
+  }
+  //button's default color has been changed
+  li.slick-active button:before {
+    color: white;
+  }
+  .slick-list {
+    overflow: visible;
+  }
+  button {
+    z-index: 1;
+  }
+`;
+const Wrap = styled.div`
+  img {
+    border: 4px solid transparent;
+    width: 100%;
+    height: 100%;
+    bordr-radius: 6px;
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 /73%) 0 16px 10px -10px;
+    transition-duration: 300ms;
+    &:hover {
+      border: 4px solid rgba(249, 249, 249, 0.8);
+    }
+  }
+`;
