dbc952c99df0a0f5ede9e345756e24190f8e9376
Jaseela99 <jaseelachittur@gmail.com>
2022-03-14 11:38:07 +0530

    corrected

--- imdbclone/src/firebase.js
+++ imdbclone/src/firebase.js
@@ -18,6 +18,6 @@ export const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
-export const storage = firebase.storage();
+
 
 export default db;
